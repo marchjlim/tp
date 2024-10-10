ff433451dfe6cda7ff53d11c72eb5102c1758979
marchjlim <marchjlim@gmail.com>
2024-10-10 12:51:37 +0800

    Add tests and refactored command

---
+++ src/test/java/seedu/address/commons/core/Messages.java
@@ -0,0 +1,8 @@
+package seedu.address.commons.core;
+
+public class Messages {
+
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "invalid command";
+
+    public static final String MESSAGE_UNKNOWN_COMMAND = "unknown command";
+}
